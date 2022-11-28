2a419361a718e812997f691d4e599b7a83539ed5
Jesus-7777 <jesussfrannardi777@gmail.com>
2022-11-28 09:43:49 -0500

    clase3 Completada

--- index.js
+++ index.js
@@ -4,11 +4,23 @@ console.log(btn);
 
 
 //capturar eventos o listeners 
-const createTask=(evento)=>{
- evento.preventDefault();//Este metodo no recarga la pagina
+const createTask = (evento) => {
+ evento.preventDefault(); //Este metodo no recarga la pagina
  const input = document.querySelector("[data-form-input]");
- console.log(input.value);
+ const value = input.value;
+ const list = document.querySelector("[data-list]");
+ const task = document.createElement("li");
+ task.classList.add("card");
+ input.value = "";
+ const content = `<div>
+ <i class="far fa-check-square icon"></i>
+ <span class="task">${value}</span>
+</div>
+<i class="fas fa-trash-alt trashIcon icon"></i>`;
+ task.innerHTML = content;
+
+ list.appendChild(task)
+
 }
 //Arrow funtion (funciones flechas) o funciones anonimas 
-btn.addEventListener("click",createTask);
-
+btn.addEventListener("click", createTask);
